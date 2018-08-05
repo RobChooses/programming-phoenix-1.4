3fc8e708caec2fe2b8406e9a1251a3dbc6e8fc13
Robert Chu <RobChooses@users.noreply.github.com>
2018-08-05 18:26:50 +0100

    Chapter 6

--- lib/rumbl_web.ex
+++ lib/rumbl_web.ex
@@ -23,6 +23,7 @@ defmodule RumblWeb do
 
       import Plug.Conn
       import RumblWeb.Gettext
+      import RumblWeb.Auth, only: [authenticate_user: 2]
       alias RumblWeb.Router.Helpers, as: Routes
     end
   end
@@ -49,6 +50,7 @@ defmodule RumblWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
+      import RumblWeb.Auth, only: [authenticate_user: 2]
     end
   end
 
